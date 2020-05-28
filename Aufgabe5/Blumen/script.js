ded8dd5d5bd32092221ccf6365b80ae91a05db15
carolinbng <64132125+carolinbng@users.noreply.github.com>
2020-05-28 21:18:07 +0200

    neu

--- Aufgabe5/Blumen/script.js
+++ Aufgabe5/Blumen/script.js
@@ -1,35 +1,41 @@
 "use strict";
 var Aufgabe5;
 (function (Aufgabe5) {
-    // Das ist der erste Artikel
-    // Er unterscheidet sich von dem Rest
-    let artikelUeberschrift = document.createElement("h2");
-    artikelUeberschrift.innerHTML = "Frische Blumen";
-    artikelUeberschrift.setAttribute("id", "Überschrift");
-    let divBlume = document.createElement("div");
-    divBlume.setAttribute("class", "blume");
-    let imgBlume = document.createElement("img");
-    imgBlume.setAttribute("src", Aufgabe5.blumen[0].bild);
-    imgBlume.setAttribute("alt", "toller Blumenstrauss");
-    let artikelName = document.createElement("h3");
-    artikelName.innerHTML = Aufgabe5.blumen[0].name;
-    let artikelBeschreibung = document.createElement("ul");
-    let artikelBeschreibungInhalt = document.createElement("li");
-    artikelBeschreibungInhalt.innerHTML = Aufgabe5.blumen[0].beschreibung;
-    artikelBeschreibung.appendChild(artikelBeschreibungInhalt);
-    let breakElement = document.createElement("br");
-    let pPreis = document.createElement("p");
-    pPreis.setAttribute("class", "Preis");
-    pPreis.innerHTML = Aufgabe5.blumen[0].preis;
-    let buttonWarenkorb = document.createElement("button");
-    buttonWarenkorb.innerHTML = "In den Warenkorb";
-    divBlume.appendChild(imgBlume);
-    divBlume.appendChild(artikelName);
-    divBlume.appendChild(artikelBeschreibung);
-    divBlume.appendChild(breakElement);
-    divBlume.appendChild(pPreis);
-    divBlume.appendChild(buttonWarenkorb);
-    document.getElementById("artikelListe")?.appendChild(artikelUeberschrift);
-    document.getElementById("artikelListe")?.appendChild(divBlume);
+    // Artikelliste
+    for (let i = 0; i < Aufgabe5.blumen.length; i++) {
+        let artikelUeberschrift = document.createElement("h2");
+        if (i == 0) {
+            artikelUeberschrift.innerHTML = "Frische Blumen";
+        }
+        else {
+            artikelUeberschrift.innerHTML = "✿";
+        }
+        artikelUeberschrift.setAttribute("id", "Überschrift");
+        let divBlume = document.createElement("div");
+        divBlume.setAttribute("class", "blume");
+        let imgBlume = document.createElement("img");
+        imgBlume.setAttribute("src", Aufgabe5.blumen[i].bild);
+        imgBlume.setAttribute("alt", "toller Blumenstrauss");
+        let artikelName = document.createElement("h3");
+        artikelName.innerHTML = Aufgabe5.blumen[i].name;
+        let artikelBeschreibung = document.createElement("ul");
+        let artikelBeschreibungInhalt = document.createElement("li");
+        artikelBeschreibungInhalt.innerHTML = Aufgabe5.blumen[i].beschreibung;
+        artikelBeschreibung.appendChild(artikelBeschreibungInhalt);
+        let breakElement = document.createElement("br");
+        let pPreis = document.createElement("p");
+        pPreis.setAttribute("class", "Preis");
+        pPreis.innerHTML = Aufgabe5.blumen[i].preis;
+        let buttonWarenkorb = document.createElement("button");
+        buttonWarenkorb.innerHTML = "In den Warenkorb";
+        divBlume.appendChild(imgBlume);
+        divBlume.appendChild(artikelName);
+        divBlume.appendChild(artikelBeschreibung);
+        divBlume.appendChild(breakElement);
+        divBlume.appendChild(pPreis);
+        divBlume.appendChild(buttonWarenkorb);
+        document.getElementById("artikelListe")?.appendChild(artikelUeberschrift);
+        document.getElementById("artikelListe")?.appendChild(divBlume);
+    }
 })(Aufgabe5 || (Aufgabe5 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe5/Blumen/script.ts
+++ Aufgabe5/Blumen/script.ts
@@ -1,46 +1,50 @@
 namespace Aufgabe5 {
 
-    // Das ist der erste Artikel
-    // Er unterscheidet sich von dem Rest
-    let artikelUeberschrift: HTMLElement = document.createElement("h2");
-    artikelUeberschrift.innerHTML = "Frische Blumen";
-    artikelUeberschrift.setAttribute("id", "Überschrift");
+    // Artikelliste
+    for (let i: number = 0; i < blumen.length; i++) {
+        let artikelUeberschrift: HTMLElement = document.createElement("h2");
+        if (i == 0) {
+            artikelUeberschrift.innerHTML = "Frische Blumen";
+        } else {
+            artikelUeberschrift.innerHTML = "✿";
+        }
+ 
+        artikelUeberschrift.setAttribute("id", "Überschrift");
+        
+        let divBlume: HTMLElement = document.createElement("div");
+        divBlume.setAttribute("class", "blume");
+
+        let imgBlume: HTMLElement = document.createElement("img");
+        imgBlume.setAttribute("src", blumen[i].bild);
+        imgBlume.setAttribute("alt", "toller Blumenstrauss");
+
+        let artikelName: HTMLElement = document.createElement("h3");
+        artikelName.innerHTML = blumen[i].name;
+
+        let artikelBeschreibung: HTMLElement = document.createElement("ul");
+        let artikelBeschreibungInhalt: HTMLElement = document.createElement("li");
+        artikelBeschreibungInhalt.innerHTML = blumen[i].beschreibung;
+        artikelBeschreibung.appendChild(artikelBeschreibungInhalt);
+
+        let breakElement: HTMLElement = document.createElement("br");
+
+        let pPreis: HTMLElement = document.createElement("p");
+        pPreis.setAttribute("class" , "Preis");
+        pPreis.innerHTML = blumen[i].preis;
+
+        let buttonWarenkorb: HTMLElement = document.createElement("button");
+        buttonWarenkorb.innerHTML = "In den Warenkorb";
+
     
-    let divBlume: HTMLElement = document.createElement("div");
-    divBlume.setAttribute("class", "blume");
+        divBlume.appendChild(imgBlume);
+        divBlume.appendChild(artikelName);
+        divBlume.appendChild(artikelBeschreibung);
+        divBlume.appendChild(breakElement);
+        divBlume.appendChild(pPreis);
+        divBlume.appendChild(buttonWarenkorb);
 
-    let imgBlume: HTMLElement = document.createElement("img");
-    imgBlume.setAttribute("src", blumen[0].bild);
-    imgBlume.setAttribute("alt", "toller Blumenstrauss");
-
-    let artikelName: HTMLElement = document.createElement("h3");
-    artikelName.innerHTML = blumen[0].name;
-
-    let artikelBeschreibung: HTMLElement = document.createElement("ul");
-    let artikelBeschreibungInhalt: HTMLElement = document.createElement("li");
-    artikelBeschreibungInhalt.innerHTML = blumen[0].beschreibung;
-    artikelBeschreibung.appendChild(artikelBeschreibungInhalt);
-
-    let breakElement: HTMLElement = document.createElement("br");
-
-    let pPreis: HTMLElement = document.createElement("p");
-    pPreis.setAttribute("class" , "Preis");
-    pPreis.innerHTML = blumen[0].preis;
-
-    let buttonWarenkorb: HTMLElement = document.createElement("button");
-    buttonWarenkorb.innerHTML = "In den Warenkorb";
-
-   
-    divBlume.appendChild(imgBlume);
-    divBlume.appendChild(artikelName);
-    divBlume.appendChild(artikelBeschreibung);
-    divBlume.appendChild(breakElement);
-    divBlume.appendChild(pPreis);
-    divBlume.appendChild(buttonWarenkorb);
-
-       
-    document.getElementById("artikelListe")?.appendChild(artikelUeberschrift);
-    document.getElementById("artikelListe")?.appendChild(divBlume);
-
-
+        
+        document.getElementById("artikelListe")?.appendChild(artikelUeberschrift);
+        document.getElementById("artikelListe")?.appendChild(divBlume);
+    }
 }
