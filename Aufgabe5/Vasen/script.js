0e9b289bc67c7124747973d1676dbb6b025c2fde
carolinbng <64132125+carolinbng@users.noreply.github.com>
2020-05-28 11:17:51 +0200

    neu

--- Aufgabe5/Vasen/script.js
+++ Aufgabe5/Vasen/script.js
@@ -1,2 +1,88 @@
 "use strict";
 //# sourceMappingURL=script.js.map
+
+let vase1 = {
+    bild:"vase1.jpg",
+    name:"Vase Notches",
+    beschreibung:"Vase aus Glas. Höhe 20cm.",
+    preis:"15,00€"
+};
+
+let vase2 = {
+    bild:"vase2.jpg",
+    name:"Vase Virbel",
+    beschreibung:"Tolle Vase aus Porzellan in der Farbe beige.",
+    preis:"14,99€"
+};
+
+let vase3 = {
+    bild:"vase3.jpg",
+    name:"Vase Notcha",
+    beschreibung:"Vase Notcha in Kugelform. Farbe korall.",
+    preis:"29,95€"
+};
+
+let vase4 = {
+    bild:"vase4.jpg",
+    name:"Vase long",
+    beschreibung:"Vase long in einer eleganten Form. Aus Glas; Höhe 23cm",
+    preis:"19,99€"
+};
+
+let vase5 = {
+    bild:"vase5.jpg",
+    name:"Dekovase Ethno",
+    beschreibung:"Dekovase Ethno mit speziellem Design. Aus Porzellan; Höhe 19cm",
+    preis:"13,99€"
+};
+
+let vase6 = {
+    bild:"vase6.jpg",
+    name:"Bodenvase Wirbel",
+    beschreibung:"Tolle Vase aus Glas mit einem leichten, pinken Touch.",
+    preis:"18,95€"
+};
+
+let vase7 = {
+    bild:"vase7.jpg",
+    name:"Vase Ministripe",
+    beschreibung:"Vase Ministripe zum Aufhängen. Perfekt für Abende im Garten.",
+    preis:"13,00€"
+};
+
+let vase8 = {
+    bild:"vase8.jpg",
+    name:"Marokko Vase",
+    beschreibung:"Außergewöhnliches Muster hat diese Marokkanische Vase.",
+    preis:"14,95€"
+};
+
+let vase9 = {
+    bild:"vase9.jpg",
+    name:"Vase Shine",
+    beschreibung:"Stylische Vase in beige. Porzellan; Höhe 8cm",
+    preis:"16,99€"
+};
+
+let vase10 = {
+    bild:"vase10.jpg",
+    name:"Vase Coconut",
+    beschreibung:"Vase Coconut in geripptem Muster. Ein Hingucker.",
+    preis:"19,99€"
+};
+
+let vase11 = {
+    bild:"vase11.jpg",
+    name:"Vasen Fine Stripe",
+    beschreibung:"Vasen im 3er Set. Glas; Höhe 11cm",
+    preis:"18,99€"
+};
+
+let vase12 = {
+    bild:"vase12.jpg",
+    name:"Vase Monday Funday",
+    beschreibung:"Vase Monday Funday mit außergewöhnlichen Glas-Muster. Höhe 18cm",
+    preis:"12,95€"
+};
+
+
