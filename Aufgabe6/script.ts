ff4501c6ad245a1d06553a9c31d59443e594e846
carolinbng <64132125+carolinbng@users.noreply.github.com>
2020-06-06 15:41:13 +0200

    neu

--- Aufgabe6/script.ts
+++ Aufgabe6/script.ts
@@ -54,44 +54,44 @@ namespace Aufgabe6 {
         document.getElementById("artikelListe")?.appendChild(artikelUeberschrift);
         document.getElementById("artikelListe")?.appendChild(divArtikel);
     }
-}
+
 
 //Ab hier Aufgabe06.01
 
 
-let warenkorbTotal: number = 0;
-let artikelZaehler: number = 0;
+    let warenkorbTotal: number = 0;
+    let artikelZaehler: number = 0;
 //let zaehlerAnzeigen: HTMLParagraphElement = document.createElement("p");
-let divBlase: HTMLDivElement = document.createElement("div");
-divBlase.id = "blase";
+    let divBlase: HTMLDivElement = document.createElement("div");
+    divBlase.id = "blase";
 
-function handleClick(_event: Event): void  {
-    
-    //if (artikelZaehler >= 0) {
-      //  document.getElementById("artikelBlase")?.appendChild(divBlase);
-    //}
+    function handleClick(_event: Event): void  {
+        
+        //if (artikelZaehler >= 0) {
+        //  document.getElementById("artikelBlase")?.appendChild(divBlase);
+        //}
 
-    //Zahl in der Blase
+        //Zahl in der Blase
 
-    if (artikelZaehler < 1) {
-        document.getElementById("header")?.appendChild(divBlase);
-    }
-    artikelZaehler += 1;
-    divBlase.innerHTML = "" + artikelZaehler;
-
-
-
-    //artikelZaehler++;
-    //zaehlerAnzeigen.innerHTML = "" + artikelZaehler;
-    //document.getElementById("divBlase")?.appendChild(zaehlerAnzeigen);
-
-    //Summe ausrechnen
-    
-}
+        if (artikelZaehler < 1) {
+            document.getElementById("header")?.appendChild(divBlase);
+        }
+        artikelZaehler += 1;
+        divBlase.innerHTML = "" + artikelZaehler;
 
 
 
+        //artikelZaehler++;
+        //zaehlerAnzeigen.innerHTML = "" + artikelZaehler;
+        //document.getElementById("divBlase")?.appendChild(zaehlerAnzeigen);
+
+        //Summe ausrechnen
+        
+    }}
 
 
-//Ab hier Aufgabe06.02
+
+
+
+    //Ab hier Aufgabe06.02
 
