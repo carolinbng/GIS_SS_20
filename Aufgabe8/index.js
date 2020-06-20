c8fd7d26a32a1a5db3ad105a9e387cfb6b016d95
carolinbng <64132125+carolinbng@users.noreply.github.com>
2020-06-20 08:54:13 +0200

    new

--- Aufgabe8/index.js
+++ Aufgabe8/index.js
@@ -5,7 +5,7 @@ var Aufgabe8;
     //Hängt Formulardaten an URL
     async function submit() {
         let formData = new FormData(document.forms[0]);
-        let url = "http://127.0.0.1:8100";
+        let url = "https://testgisss2020.herokuapp.com/";
         let query = new URLSearchParams(formData);
         url = url + "?" + query.toString();
         await fetch(url);
